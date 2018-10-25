19923ab5d8b40762c4dfe17f3d0626bf686076bb
Jacco Goris <jacco@around.media>
2018-10-25 10:40:05 +0200

    cleanup

--- src/FavoriteMovies.js
+++ src/FavoriteMovies.js
@@ -1,25 +1,19 @@
-import React, { PureComponent } from "react";
+import React from 'react';
 
-class FavoriteMovies extends PureComponent {
-  render() {
-    const { profiles, users, movies } = this.props;
-
+export default (FavoriteMovies = ({ profiles, users, movies }) => {
     const profilesList = profiles.map(profile => {
-      const { id, userID, favoriteMovieID } = profile;
-      const currentUser = users[userID];
-      const favoriteMovie = movies[favoriteMovieID];
-      const favoriteMovieString = `${currentUser.name}'s favorite movie is ${
-        favoriteMovie.name
-      }`;
-      return (
-        <li key={id}>
-          <p>{favoriteMovieString}</p>
-        </li>
-      );
+        const { id, userID, favoriteMovieID } = profile;
+        const currentUser = users[userID];
+        const favoriteMovie = movies[favoriteMovieID];
+        const favoriteMovieString = `${currentUser.name}'s favorite movie is ${
+            favoriteMovie.name
+        }`;
+        return (
+            <li key={id}>
+                <p>{favoriteMovieString}</p>
+            </li>
+        );
     });
 
     return <ul>{profilesList}</ul>;
-  }
-}
-
-export default FavoriteMovies;
+});
